19bfdad7e7e3632e385d1e7efdc4c5913e577e59
Mitch Flax <mitchell.flax@gmail.com>
2024-10-22 21:22:45 -0400

    Started 1-23

---
+++ KR1-23/decomment.c
@@ -0,0 +1,14 @@
+#include "decomment.h"
+
+int main()
+{
+	int c, cursor;
+	bool inString, inMultilineComment, inLineComment;
+	
+	while ((c = getchar()) != EOF)
+	{
+		// this is going to get a bit complicated
+
+	}
+
+}
---
+++ KR1-23/decomment.h
@@ -0,0 +1,3 @@
+#include <stdio.h>
+
+void putBuffer(char buffer[], int startIndex, int endIndex);
